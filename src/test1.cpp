11ec05a35533d940d56d83f2f545a0175129711c
Andrew Black <ab4@comcast.net>
2017-02-25 19:44:25 -0500

    Added GLEW. Updated gitignore. Updated test1.cpp to initialize GLEW.

--- src/test1.cpp
+++ src/test1.cpp
@@ -1,21 +1,31 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include <iostream>
+#include <exception>
+
 int main(void) {
+	// Create window
 	GLFWwindow *window;
 	if (!glfwInit()) {
 		return -1;
 	}
-
 	window = glfwCreateWindow(640, 480, "Hello World!", NULL, NULL);
 	if (!window) {
 		glfwTerminate();
 		return -1;
 	}
-
 	glfwMakeContextCurrent(window);
-
+	// Initialize GLEW
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK) {
+		throw std::exception("Failed to initialize GLEW");
+	}
+	// Application loop
 	while (!glfwWindowShouldClose(window)) {
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(1.f, 1.f, 1.f, 1.f); // Set color to white
+		glClear(GL_COLOR_BUFFER_BIT); // Clear buffer to set clear color
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}

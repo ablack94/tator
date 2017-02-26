f6b8d2566a94623eb232a8680637fc58e981e9af
Andrew Black <ab4@comcast.net>
2017-02-25 20:58:26 -0500

    Implemented the hello triangle tutorial.

--- src/test1.cpp
+++ src/test1.cpp
@@ -5,6 +5,68 @@
 #include <iostream>
 #include <exception>
 
+class TatorException : std::exception {
+public:
+	TatorException(const char *msg) : std::exception(msg) {}
+};
+
+// Shaders
+GLchar *vertex_shader_source =
+"#version 330 core\n\
+layout (location=0) in vec3 position;\n\
+void main(){\n\
+gl_Position = vec4(position.x, position.y, position.z, 1.0);\n\
+}\0";
+
+GLchar *fragment_shader_source =
+"#version 330 core\n\
+out vec4 color;\n\
+void main(){\n\
+color=vec4(1.0f,1.0f,1.0f,1.0f);\n\
+}\n";
+
+void compileShader(GLuint id, GLchar *src) {
+	glShaderSource(id, 1, &src, NULL);
+	glCompileShader(id);
+	GLint success;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &success);
+	GLchar infoLog[512];
+	if (!success) {
+		glGetShaderInfoLog(id, sizeof(infoLog), NULL, infoLog);
+		throw TatorException(infoLog);
+	}
+}
+
+GLuint compileVertexShader(GLchar *src) {
+	GLuint id = glCreateShader(GL_VERTEX_SHADER);
+	compileShader(id, src);
+	return id;
+}
+
+GLuint compileFragmentShader(GLchar *src) {
+	GLuint id = glCreateShader(GL_FRAGMENT_SHADER);
+	compileShader(id, src);
+	return id;
+}
+
+GLuint createShaderProgram(std::initializer_list<GLuint> shaders) {
+	// Create the shader program
+	GLuint shader_program = glCreateProgram();
+	for (auto shader : shaders) {
+		glAttachShader(shader_program, shader);
+	}
+	glLinkProgram(shader_program);
+	// Validate the shader program
+	GLint success;
+	GLchar infoLog[512];
+	glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shader_program, 512, NULL, infoLog);
+		throw TatorException(infoLog);
+	}
+	return shader_program;
+}
+
 int main(void) {
 	// Create window
 	GLFWwindow *window;
@@ -20,16 +82,75 @@ int main(void) {
 	// Initialize GLEW
 	glewExperimental = GL_TRUE;
 	if (glewInit() != GLEW_OK) {
-		throw std::exception("Failed to initialize GLEW");
+		throw TatorException("Failed to initialize GLEW");
 	}
+	// Compile shaders
+	GLuint fragment_shader = compileFragmentShader(fragment_shader_source);
+	GLuint vertex_shader = compileVertexShader(vertex_shader_source);
+	// Make shader program
+	GLuint sprog = createShaderProgram({vertex_shader, fragment_shader});
+	glDeleteShader(fragment_shader);
+	glDeleteShader(vertex_shader);
+
+	// Data
+	GLfloat vertices[] = {
+		0.5f, 0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f,
+		-0.5f, 0.5f, 0.0f
+	};
+	GLuint indices[] = {
+		0, 1, 3,
+		1, 2, 3
+	};
+	// Make shapes
+	GLuint VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+	
+	glBindVertexArray(VAO);
+		// Send vertices to GPU
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+			glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+			// Send indices of shape to GPU
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+			glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+			// Determine what data is sent to the vertex shader
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (GLvoid*)0);
+			glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, 0); // unbind
+	glBindVertexArray(0); // unbind
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // wireframe
+
 	// Application loop
 	while (!glfwWindowShouldClose(window)) {
-		glClearColor(1.f, 1.f, 1.f, 1.f); // Set color to white
+		glfwPollEvents(); // Check events
+		/* RENDERING */
+		// Clear backbuffer
+		glClearColor(0.5f, 0.5f, 0.5f, 1.f); // Set color to white
 		glClear(GL_COLOR_BUFFER_BIT); // Clear buffer to set clear color
+		// Draw
+		glUseProgram(sprog);
+		glBindVertexArray(VAO); // bind
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0); // unbind
+
+		// Display
 		glfwSwapBuffers(window);
-		glfwPollEvents();
 	}
 
 	glfwTerminate();
 	return 0;
+}
+
+int _main(void) {
+	try {
+		return _main();
+	}
+	catch (std::exception& exc) {
+		std::cerr << "Unhandled exception: " << exc.what() << std::endl;
+		system("pause");
+	}
 }
